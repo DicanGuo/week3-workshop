8864ca426d43c614eec63fa7af6518afde20f96d
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-08-05 09:51:43 +1000

    server setup success

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 // provide http functionality
 var http = require('http').Server(app);
 
-app.use(express.static(_dirname + '/www'));
+app.use(express.static(__dirname + '/www'));
 
-http.listen(3000);
+let server = http.listen(3000, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("My First Nodejs Server!");
+    console.log("Server listening on: "+ host + " port: " + port);
+});
